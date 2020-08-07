50c87752265d7d966a03c8d170afe96f20dbab7e
sajjadhussains <sajjadhussain14.bd@gmail.com>
2020-08-08 01:01:53 +0600

    templete in Es6

---
+++ templete.js
@@ -0,0 +1,19 @@
+// const firstName ="Justin";
+// const lastName = "TimberLake";
+// const fullName = firstName + " "+lastName + " is a good boy";
+// console.log(fullName);
+
+//the same work can do by Es6 more easily....
+// const fullName =`${firstName} ${lastName} is a good boy ${50+50}`;
+// console.log(fullName);
+
+// Normaly how we write multiple line in javascript is given bellow:
+
+const multiLine ="I love you\n"+"I miss you\n"+"I need you";
+console.log(multiLine);
+
+//same work is more easy by Es6---
+const multipleLine=`I love Bangladesh
+I love my parents
+I love my little sister`
+console.log(multipleLine);
